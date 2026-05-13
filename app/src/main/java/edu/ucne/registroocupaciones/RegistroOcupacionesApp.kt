00c535981f4c5fc7836dca671cef97dfefd67e8c
James De Peña <jamesdepena0@gmail.com>
2026-05-13 14:52:51 -0400

    Agrega clase de aplicación Hilt

---
+++ app/src/main/java/edu/ucne/registroocupaciones/RegistroOcupacionesApp.kt
@@ -0,0 +1,8 @@
+package edu.ucne.registroocupaciones
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class RegistroOcupacionesApp : Application() {
+}
