8eb62d7ccf01255a8a950fbf0ca2f7d6bd0b9317
James De Peña <jamesdepena0@gmail.com>
2026-05-15 19:42:51 -0400

    Agrega el EmleadoEntity y EmpleadoDao

--- app/src/main/java/edu/ucne/registroocupaciones/data/db/OcupacionDatabase.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/db/OcupacionDatabase.kt
@@ -2,16 +2,20 @@ package edu.ucne.registroocupaciones.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import edu.ucne.registroocupaciones.data.local.dao.EmpleadoDao
 import edu.ucne.registroocupaciones.data.local.dao.OcupacionDao
+import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
 import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
 
 @Database(
     entities = [
-        OcupacionEntity::class
+        OcupacionEntity::class,
+        EmpleadoEntity::class
     ],
-    version = 1,
+    version = 2,
     exportSchema = false
 )
 abstract class OcupacionDatabase : RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
+    abstract fun empleadoDao(): EmpleadoDao
 }
