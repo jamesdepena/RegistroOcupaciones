d84f730e0de01356afea79ed6a539786b1785f48
James De Peña <jamesdepena0@gmail.com>
2026-05-16 20:14:58 -0400

    Agrega funcion para conseguir empleado por el nombre

--- app/src/main/java/edu/ucne/registroocupaciones/data/local/dao/EmpleadoDao.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/local/dao/EmpleadoDao.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Query
 import androidx.room.Upsert
 import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
+import edu.ucne.registroocupaciones.domain.empleados.model.Empleado
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -20,4 +21,7 @@ interface EmpleadoDao {
 
     @Query("DELETE FROM empleados WHERE empleadoId = :id")
     suspend fun deleteById(id: Int)
+
+    @Query("SELECT * FROM empleados WHERE LOWER(nombres) = LOWER(:nombre)")
+    suspend fun getByNombre(nombre: String): EmpleadoEntity?
 }
