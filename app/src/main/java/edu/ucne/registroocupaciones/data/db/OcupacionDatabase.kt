4a55208f831d1a4a5b95ed7478615ff906f2af97
James De Peña <jamesdepena0@gmail.com>
2026-05-17 01:01:40 -0400

    Agrega converters para la fecha para database

--- app/src/main/java/edu/ucne/registroocupaciones/data/db/OcupacionDatabase.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/data/db/OcupacionDatabase.kt
@@ -2,10 +2,13 @@ package edu.ucne.registroocupaciones.data.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 import edu.ucne.registroocupaciones.data.local.dao.EmpleadoDao
 import edu.ucne.registroocupaciones.data.local.dao.OcupacionDao
 import edu.ucne.registroocupaciones.data.local.entities.EmpleadoEntity
 import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
+import java.time.LocalDate
 
 @Database(
     entities = [
@@ -15,7 +18,20 @@ import edu.ucne.registroocupaciones.data.local.entities.OcupacionEntity
     version = 2,
     exportSchema = false
 )
+@TypeConverters(DateConverters::class)
 abstract class OcupacionDatabase : RoomDatabase() {
     abstract fun ocupacionDao(): OcupacionDao
     abstract fun empleadoDao(): EmpleadoDao
+}
+
+class DateConverters {
+    @TypeConverter
+    fun fromString(value: String?): LocalDate? {
+        return value?.let { LocalDate.parse(it) }
+    }
+
+    @TypeConverter
+    fun dateToString(date: LocalDate?): String? {
+        return date?.toString()
+    }
 }
