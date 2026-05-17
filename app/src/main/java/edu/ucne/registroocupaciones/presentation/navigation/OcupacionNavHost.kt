d485cc68ab0c3edcdb696a2c9933b4460a4e90ad
James De Peña <jamesdepena0@gmail.com>
2026-05-17 01:09:52 -0400

    Implementa el drawer y las pantallas de empleado en el navHost

--- app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
+++ app/src/main/java/edu/ucne/registroocupaciones/presentation/navigation/OcupacionNavHost.kt
@@ -1,40 +1,77 @@
 package edu.ucne.registroocupaciones.presentation.navigation
 
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
+import edu.ucne.registroocupaciones.presentation.empleado.edit.EditEmpleadoScreen
+import edu.ucne.registroocupaciones.presentation.empleado.list.EmpleadoListScreen
 import edu.ucne.registroocupaciones.presentation.ocupacion.edit.EditOcupacionScreen
 import edu.ucne.registroocupaciones.presentation.ocupacion.list.OcupacionListScreen
+import kotlinx.coroutines.launch
 
 @Composable
 fun OcupacionNavHost(
     navController: NavHostController = rememberNavController()
 ) {
-    NavHost(
-        navController = navController,
-        startDestination = Screen.OcupacionList
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+
+    DrawerMenu(
+        drawerState = drawerState,
+        navHostController = navController
     ) {
-        composable<Screen.OcupacionList> {
-            OcupacionListScreen(
-                goToOcupacion = { id ->
-                    navController.navigate(Screen.Ocupacion(id))
-                },
-                createOcupacion = {
-                    navController.navigate(Screen.Ocupacion(0))
-                }
+        NavHost(
+            navController = navController,
+            startDestination = Screen.OcupacionList
+        ) {
+            composable<Screen.OcupacionList> {
+                OcupacionListScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    goToOcupacion = { id ->
+                        navController.navigate(Screen.Ocupacion(id))
+                    },
+                    createOcupacion = {
+                        navController.navigate(Screen.Ocupacion(0))
+                    }
 
-            )
-        }
+                )
+            }
 
-        composable<Screen.Ocupacion> {
-            val args = it.toRoute<Screen.Ocupacion>()
-            EditOcupacionScreen(
-                ocupacionId = args.ocupacionId,
-                onNavigateBack = { navController.navigateUp() },
-            )
+            composable<Screen.Ocupacion> {
+                val args = it.toRoute<Screen.Ocupacion>()
+                EditOcupacionScreen(
+                    ocupacionId = args.ocupacionId,
+                    onNavigateBack = { navController.navigateUp() },
+                    onDrawer = { scope.launch { drawerState.open() } },
+                )
+            }
+
+            composable<Screen.EmpleadoList> {
+                EmpleadoListScreen(
+                    onDrawer = { scope.launch { drawerState.open() } },
+                    goToEmpleado = { id ->
+                        navController.navigate(Screen.EmpleadoForm(id))
+                    },
+                    createEmpleado = {
+                        navController.navigate(Screen.EmpleadoForm(0))
+                    }
+                )
+            }
+
+            composable<Screen.EmpleadoForm> {
+                val args = it.toRoute<Screen.EmpleadoForm>()
+                EditEmpleadoScreen(
+                    empleadoId = args.empleadoId,
+                    onNavigateBack = { navController.navigateUp() },
+                    onDrawer = { scope.launch { drawerState.open() } },
+                )
+            }
         }
     }
 }
